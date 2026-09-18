b7ce65cecbad372384d593e4311e70e4af59677f
raedemed <1.05056653e+08+raedemed@users.noreply.github.com>
2026-09-18 16:48:59 +0000

    Update Monetag service worker zone

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11826136
+    "zoneId": 11834407
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
